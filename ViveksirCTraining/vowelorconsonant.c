61e074a628148cce958c7e87b5f22006fb341e7f
clunkiersalt817 <73820037+clunkiersalt817@users.noreply.github.com>
2021-09-14 21:16:39 +0530

    Update vowelorconsonant.c

--- ViveksirCTraining/vowelorconsonant.c
+++ ViveksirCTraining/vowelorconsonant.c
@@ -4,6 +4,6 @@ int main()
     char a;
     scanf("%c",&a);
     if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
-    printf("The character given as a")
+    printf("The character given is a")
     return 0;
 }
