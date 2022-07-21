6f94993279521f2534b5ffaef42af92ab316cb28
Kshitiz Sharma <cs20206@global.org.in>
2022-07-22 00:18:59 +0530

    added

--- leetcode/validParentheses.cpp
+++ leetcode/validParentheses.cpp
@@ -1,19 +1,51 @@
-//((s[i] == '{' || s[i + 1] == '}') || (s[i] == '[' || s[i + 1] == ']') || (s[i] == '(' || s[i + 1] == ')'))
+/*
+((s[i] == '{' || s[i + 1] == '}') || (s[i] == '[' || s[i + 1] == ']') || (s[i] == '(' || s[i + 1] == ')'))
+We use a stack (it's a LIFO structure)
+if open bracket - push
+if close bracket
+- 1. is stack empty ?
+- 2. is corresponding opening bracket available ? if yes then pop last open bracket.
+*/
 
 #include <iostream>
+#include <stack>
 using namespace std;
-bool main()
+
+bool isValid(string p)
 {
-    string s;
-    int i = 0;
-
-    cout << "Enter parentheses to check : "; // Enter only parentheses.
-    cin >> s;
-
-    if (((s[i] == '{' || s[i + 1] == '}') || (s[i] == '[' || s[i + 1] == ']') || (s[i] == '(' || s[i + 1] == ')')) )
+    stack<char> st;
+    int n = p.size();
+    char ch;
+    for (int i = 0; i < n; i++)
     {
-        return true;
+        if (p[i] == '{' || p[i] == '(' || p[i] == '[')
+        {
+            st.push(p[i]);
+        }
+        else
+        {
+            if (st.size() == 0)
+                return false;
+
+            ch = st.top();
+            st.pop();
+
+            if ((p[i] == '}' && ch == '{') || (p[i] == ']' && ch == '[') || (p[i] == ')' && ch == '('))
+            {
+                continue;
+            }
+            else
+                return false;
+        }
     }
-    else
-        return false;
+    return st.empty();
+}
+
+int main()
+{
+
+    string s;
+    cin >> s;
+    cout << isValid(s);
+    return 0;
 }
